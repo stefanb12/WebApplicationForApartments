b4820868680b0d180beea48cd7b75af4275109c9
Stefan Beljic <stefan.beljic23@gmail.com>
2020-08-19 20:36:24 +0200

    Inicijalizacija projekta

---
+++ src/beans/User.java
@@ -0,0 +1,71 @@
+package beans;
+
+public class User {
+	private String username;
+	private String password;
+	private String name;
+	private String surname;
+	private Boolean gender; // True = man, False = Woman
+	private Role role;
+	
+	public User() {}
+	
+	public User(String username, String password, String name, String surname, Boolean gender, Role role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+		this.surname = surname;
+		this.gender = gender;
+		this.role = role;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Boolean getGender() {
+		return gender;
+	}
+
+	public void setGender(Boolean gender) {
+		this.gender = gender;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}	
+	
+}
---
+++ src/services/LoginService.java
@@ -0,0 +1,8 @@
+package services;
+
+import javax.ws.rs.Path;
+
+@Path("") // Promeni putanju
+public class LoginService {
+
+}
